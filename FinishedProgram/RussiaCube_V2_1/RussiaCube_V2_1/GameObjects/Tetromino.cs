d9d18a83e96947a87a31fd926b9393fedbce4db8
YaoZhongCode <xisiteliyanvwang@gmail.com>
2026-08-07 01:01:39 +0530

    feat: implement tetromino composition
    - Implement Tetromino as a collection of four Block objects
    - Encapsulate block management inside Tetromino
    - Expose blocks as a read-only collection
    - Lay the foundation for rotation behavior

---
+++ FinishedProgram/RussiaCube_V2_1/RussiaCube_V2_1/GameObjects/Tetromino.cs
@@ -0,0 +1,35 @@
+﻿using RussiaCube_V2_1.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RussiaCube_V2_1.GameObjects
+{
+    /// <summary>
+    /// 游戏主角！俄罗斯方块
+    /// </summary>
+    internal class Tetromino
+    {
+        //内部集合
+        private List<Block> _blocks;
+
+        //供外部只读取，无法修改
+        public IReadOnlyList<Block> Blocks => _blocks;
+
+        //方块类型
+        public TetrominoType Type { get; }
+        public Tetromino(TetrominoType type)
+        {
+            Type = type;
+
+            //初始化方块
+            _blocks = new List<Block>()
+            {
+                new Block(new Position(0, 0), type),
+                new Block(new Position(0, 0), type),
+                new Block(new Position(0, 0), type),
+                new Block(new Position(0, 0), type)
+            };
+        }
+    }
+}
