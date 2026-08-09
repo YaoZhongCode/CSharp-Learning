2bfcf7f3c913812291723c195db9c8e6b78ad86f
YaoZhongCode <xisiteliyanvwang@gmail.com>
2026-08-10 02:38:05 +0530

    feat: implement row clearing
    - Add full row detection to Map
    - Remove completed rows and shift blocks downward
    - Support clearing multiple rows in a single operation
    - Return the number of rows cleared

--- FinishedProgram/RussiaCube_V2_1/RussiaCube_V2_1/Maps/Map.cs
+++ FinishedProgram/RussiaCube_V2_1/RussiaCube_V2_1/Maps/Map.cs
@@ -58,5 +58,74 @@ namespace RussiaCube_V2_1.Maps
             //如果为空，返回false(未被占用)
             return false;
         }
+
+        /// <summary>
+        /// 清理满行的地图
+        /// </summary>
+        /// <returns>返回消除的行数</returns>
+        public int ClearFullRows()
+        {
+            //记录消除的行数
+            int rowsCleared = 0;
+
+            //从最地下一行开始往上遍历
+            for(int y = Height - 1; y >= 0; y--)
+            {
+                //如果某一行满了
+                if (IsRowFull(y))
+                {
+                    //整体下移
+                    ShiftRowDown(y);
+                    //计数消除行数
+                    rowsCleared++;
+
+                    //因为消除了这一行，它上面的格子落下来，需要重新检测一遍这一行
+                    y++;
+                }
+            }
+            return rowsCleared;
+        }
+
+        /// <summary>
+        /// 检查指定行是否满格
+        /// </summary>
+        /// <param name="y">指定的高度</param>
+        /// <returns></returns>
+        private bool IsRowFull(int y)
+        {
+            for(int x = 0; x < Width; x++)
+            {
+                //如果该高度里有任一一格空，不满行，返回false
+                if (_blocks[x, y] == null)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 将指定行之上的所有格子数据往下移动
+        /// </summary>
+        /// <param name="targetY">指定高度</param>
+        private void ShiftRowDown(int targetY)
+        {
+            //将指定行之后的每一行都下移
+            for(int y = targetY; y > 0; y--)
+            {
+                for(int x = 0; x < Width; x++)
+                {
+                    _blocks[x, y] = _blocks[x, y - 1];
+                }
+            }
+
+            //置空最上面一行
+            //（它的上面已没有方块可以下移，它本身的数据已下移，所以需要清空）
+            for(int x = 0; x < Width; x++)
+            {
+                _blocks[x, 0] = null;
+            }
+        }
     }
 }
